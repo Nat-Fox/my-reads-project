9e277aa716c3f025c949750daa4c1504ef0cf092
Nat Garrido <nat@Nats-Mac-mini.local>
2021-03-27 22:56:52 -0300

    static data to test

--- src/ListBook.js
+++ src/ListBook.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class ListBook extends React.Component {
   render() {
+    console.log('PROPS ->', this.props.title)
     return (
       <div className="list-books">
         <div className="list-books-content">
           <div>
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
+              {/* <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   <li>
@@ -47,10 +48,10 @@ class ListBook extends React.Component {
                     </div>
                   </li>
                 </ol>
-              </div>
+              </div> */}
             </div>
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
+              {/* <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   <li>
@@ -90,10 +91,10 @@ class ListBook extends React.Component {
                     </div>
                   </li>
                 </ol>
-              </div>
+              </div> */}
             </div>
             <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
+              {/* <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   <li>
@@ -151,9 +152,9 @@ class ListBook extends React.Component {
                     </div>
                   </li>
                 </ol>
-              </div>
+              </div>*/}
             </div>
-          </div>
+          </div> 
         </div>
         <div className="open-search">
           <button onClick={this.props.showSearchPage}>Add a book</button>
